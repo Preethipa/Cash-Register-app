08266555a304497c977ccf71612533e5e4781c6e
PREETHI <preethirajavel06@gmail.com>
2022-10-13 14:30:02 +0530

    input output done

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const billAmount = document.querySelector("#bill-amount");
- console.log(billAmount.value);
+const cashGiven = document.querySelector("#cash-given");
+const checkBotton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
 
+checkBotton.addEventListener("click", function validateBillandCashAmount(){
+   showhideError();
+    if(billAmount.value > 0){
+        if(cashGiven.value >= billAmount.value){
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculate(amountToBeReturned);
 
+        } else{
+            showError("Cash should  atleast be equal to bill amount");
+
+        }
+ 
+    } else{
+        showError("Invalid amount");
+    }
+});
+
+function showhideError(msg){
+    message.style.display = "none";
+}
+
+function showError(msg){
+    message.style.display = "block";
+        message.innerText = msg;
+}
