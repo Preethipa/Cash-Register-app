95a6c07c8de26d55eea031bb2364672699c7070b
PREETHI <preethirajavel06@gmail.com>
2022-10-13 16:04:49 +0530

    Done processing

--- index.js
+++ index.js
@@ -2,23 +2,37 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkBotton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000,500,100,20,10,5,1];
 
 checkBotton.addEventListener("click", function validateBillandCashAmount(){
    showhideError();
     if(billAmount.value > 0){
         if(cashGiven.value >= billAmount.value){
             const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculate(amountToBeReturned);
+            calculateChange(amountToBeReturned);
 
         } else{
             showError("Cash should  atleast be equal to bill amount");
 
         }
- 
+  
     } else{
         showError("Invalid amount");
     }
 });
+
+function calculateChange(amountToBeReturned){
+    for( let i=0; i<availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+        );
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+
+    }
+}
 
 function showhideError(msg){
     message.style.display = "none";
